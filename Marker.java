332dd69d1c17c7ff3b4f0070b0ba997e46f18e7e
bob <bob@APCSA>
2017-08-30 13:42:45 -0700

    Added Marker class with get method

---
+++ Marker.java
@@ -0,0 +1,15 @@
+import java.awt.Color;
+public class Marker
+{
+	private Color color;
+	private boolean canWrite;
+	private boolean isOpen;
+	public Color getColor()
+	{
+		return color;
+	}
+	public boolean getCanWrite()
+	{
+		return canWrite;
+	}
+}
