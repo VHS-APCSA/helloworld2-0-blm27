11e806aaa8c3b521d5284c114748e02e5adfdefb
Bryan Moore <blm72700@gmail.com>
2017-09-14 14:26:32 -0700

    Test

---
+++ Import.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Import 
+{
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+		System.out.println("Enter an integer.");
+		int num1 = input.nextInt();
+		System.out.println("Enter another integer.");
+		int num2 = input.nextInt();
+		System.out.println("The sum of your numbers is " +(num1+num2));
+	}
+}
